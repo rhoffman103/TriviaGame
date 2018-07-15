11854ce702a2375adab8f83ff3e43cbd0e0db522
Bobby H <rhoffman103@gmail.com>
2018-07-14 21:24:58 -0400

    added timer to question cards

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,9 +7,13 @@ $(document).ready(function() {
         unanswered: 0,
 
         displayTally: function() {
-            $(".card").hide();
+            $(".questions-card").hide();
+            $(".tally-card").show();
+            $("#correct").text(this.correctAnswers);
+            $("#incorrect").text(this.incorrectAnswers);
+            $("#unanswered").text(this.unanswered);
         }
-    }
+    };
 
     var questionCard = {
         currentImage:    "",
@@ -36,7 +40,7 @@ $(document).ready(function() {
             answerList: ["Uh-oh, summersalt jump!", "Grassss, Tastes Bad", "Wubba Lubba Dub Dub!", "Shum Shum, Shlippity Dop!"],
             image:      "./assets/images/wubba-lubba.jpg"
         },{
-            question:   "What did Abradolf Lincler sacrifrifice himself for in order for rick to save the party?",
+            question:   "What did Abradolf Lincler sacrifrifice himself for to save Ricks party?",
             answerList: ["Kalaxian Crystals", "Flurbo", "Crystallized Anthenite", "Ionic Defibulizer"],
             image:      "./assets/images/rick-high.jpg"
         }],
@@ -57,16 +61,19 @@ $(document).ready(function() {
         },
 
         setImage: function() {
-            $(".card-img-top").attr({src: this.currentImage});
+            $(".img-question").attr({src: this.currentImage});
         },
 
         getQuestionCard: function() {
             if (this.totalQuestions.length == 0) {
                 player.displayTally();
+                timer.stop();
             } else {
                 this.getQuestion();
                 displayQuestionAndAnswers();
             }
+            timer.reset();
+            timer.start();
         },
 
         randomlyOrderAnswerList: function() {
@@ -78,7 +85,63 @@ $(document).ready(function() {
             }
         },
 
+    };
+
+    var timer = {
+        time: 10000,
+        intervalId: 0,
+        timeLeft: 0,
+        timerRunning: false,
+
+        reset: function() {
+            timer.time = 10000;
+            $("#timer").text("00:10");
+        },
+
+        start: function() {
+            if (!timer.timerRunning) {
+                timer.intervalId = setInterval(timer.countDown, 1000);
+                timerRunning = true;
+            }
+        },
+
+        stop: function() {
+            timer.timeLeft = (timer.time) / 1000;
+            clearInterval(timer.intervalId);
+            timerRunning = false;
+            console.log(timer.timeLeft);
+        },
+
+        countDown: function() {
+            timer.time -= 1000;
+            var convertToSeconds = timer.timeConvert(timer.time);
+            $("#timer").text(convertToSeconds);
+            if (timer.time == 0) {
+                timer.stop();
+                player.unanswered++;
+                questionCard.getQuestionCard();
+            }
+        },
+
+        timeConvert: function(t) {
+            var seconds = Math.floor(t / 1000);
+
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+            return "00:" + seconds;
+        }
     }
+
+    const resetGame = function() {
+        player.correctAnswers =   0;
+        player.incorrectAnswers = 0;
+        player.unanswered =       0;
+        questionCard.setTotalQuestionsArray();
+        questionCard.getQuestionCard();
+        $(".tally-card").hide();
+        $(".questions-card").show();
+    };
 
     const displayQuestionAndAnswers = function() {
         questionCard.setImage();
@@ -87,20 +150,24 @@ $(document).ready(function() {
         $("#choiceB").text(questionCard.currentChoices[1]);
         $("#choiceC").text(questionCard.currentChoices[2]);
         $("#choiceD").text(questionCard.currentChoices[3]);
-    }
+    };
 
     // Start Trivia Game
-    questionCard.setTotalQuestionsArray();
-    questionCard.getQuestionCard();
+    resetGame();
 
     // Events
-    $(".list-group-item").on("click", function() {
+    $(".choices").on("click", function() {
         if (questionCard.answers.includes($(this).html())) {
             player.correctAnswers++;
         } else {
             player.incorrectAnswers++;
         }
+        timer.stop();
         questionCard.getQuestionCard();
+    });
+
+    $(".try-again").on("click", function() {
+        resetGame();
     })
 
 })
